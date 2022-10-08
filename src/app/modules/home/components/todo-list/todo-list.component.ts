9fe8b7cec4f479a5e2151048e06283883a883c1a
Mend1s <andremendesskt99@gmail.com>
2022-10-08 17:08:54 -0300

    add validação de preenchimento de tasks

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    // {task: "Minha nova task", checked: true},
+    // {task: "Minha segunda task", checked: true}
+  ];
 
   constructor() { }
 
